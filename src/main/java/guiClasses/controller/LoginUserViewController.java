fed80d4375f367bf4908ea4ecfecb8251c5f9bdc
Bryan Macedo <contato.bryansm@hotmail.com>
2025-03-17 09:31:36 -0300

    Arrumei o btBackArrow da LoginUserView que não estava funcionando.

--- src/main/java/guiClasses/controller/LoginUserViewController.java
+++ src/main/java/guiClasses/controller/LoginUserViewController.java
@@ -1,9 +1,15 @@
 package guiClasses.controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -12,12 +18,36 @@ public class LoginUserViewController implements Initializable {
     private Button btEnter;
 
     @FXML
-    private void onbtEnterAction(){
+    private Button btBackArrow;
+
+    @FXML
+    private void onBtBackArrowAction(){
+        try{
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
+            Parent root = loader.load();
+
+            Scene scene = btBackArrow.getScene();
+            scene.setRoot(root);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @FXML
+    private void onBtEnterAction(){
         System.out.println("onbtEnterAction: Esta funcionando!");
     }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        Image backArrowIcon = new Image(getClass().getResourceAsStream("/imgs/icons/BackArrowIcon.png"));
+
+        ImageView backArrowIconImageView = new ImageView(backArrowIcon);
+
+        backArrowIconImageView.setFitWidth(30);
+        backArrowIconImageView.setFitHeight(30);
+
+        btBackArrow.setGraphic(backArrowIconImageView);
 
     }
 }
