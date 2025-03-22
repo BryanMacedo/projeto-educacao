7f4dc838bb75b6dfc0fc373a6fd504739b170d89
Bryan Macedo <contato.bryansm@hotmail.com>
2025-03-22 15:21:11 -0300

    Comecei a buscar as palavras no DB, exibir as palavras e suas imagens e criei a lógica principal dessa atividade.

--- src/main/java/guiClasses/controller/TestNewMainViewController.java
+++ src/main/java/guiClasses/controller/TestNewMainViewController.java
@@ -1,18 +1,32 @@
 package guiClasses.controller;
 
+import db.DB;
+import db.Exception.DbException;
+import javafx.animation.PauseTransition;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseEvent;
+import javafx.util.Duration;
+import model.entities.Word;
 
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class TestNewMainViewController implements Initializable {
+    List<Word> words = new ArrayList<>();
+
     @FXML
-    private ImageView centerImageView;
+    private ImageView MainImage;
 
     @FXML
     private Label labelWordToComplete;
@@ -91,15 +105,65 @@ public class TestNewMainViewController implements Initializable {
 
     private void VerifyWord(){
         String textLabelWordToComplete = labelWordToComplete.getText();
-        if (!textLabelWordToComplete.contains("_") && textLabelWordToComplete.equals("CASA")){
+        if (!textLabelWordToComplete.contains("_") && textLabelWordToComplete.equals(words.get(0).getFullWords())){
             labelWordToComplete.setStyle("-fx-text-fill: green;");
-        } else if (!textLabelWordToComplete.contains("_") && !textLabelWordToComplete.equals("CASA")) {
+            PauseTransition pause = new PauseTransition(Duration.seconds(0.5));
+            pause.setOnFinished(event -> {
+                words.remove(0);
+                next();
+                labelWordToComplete.setStyle("-fx-text-fill: white;");
+            });
+            pause.play();
+
+        } else if (!textLabelWordToComplete.contains("_") && !textLabelWordToComplete.equals(words.get(0).getFullWords())) {
             labelWordToComplete.setStyle("-fx-text-fill: red;");
+            PauseTransition pause = new PauseTransition(Duration.seconds(0.5));
+            pause.setOnFinished(event -> {
+                labelWordToComplete.setText(words.get(0).getWordWith_());
+                labelWordToComplete.setStyle("-fx-text-fill: white;");
+            });
+            pause.play();
+        }
+    }
+
+    private void next() {
+        if (!words.isEmpty()) {
+            labelWordToComplete.setText(words.get(0).getWordWith_());
+            Image newImage = new Image(getClass().getResourceAsStream("/imgs/icons/" + words.get(0).getImgName()));
+            MainImage.setImage(newImage);
+        } else {
+            MainImage.setImage(null);
+            labelWordToComplete.setStyle("-fx-text-fill: green;");
+            labelWordToComplete.setText("Parabéns! Você acertou todas!");
         }
     }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        Connection conn = null;
+        PreparedStatement st = null;
+        ResultSet rs = null;
 
+        try {
+            conn = DB.getConnection();
+            st = conn.prepareStatement("SELECT * FROM words");
+            rs = st.executeQuery();
+
+            while (rs.next()){
+                Word newWord = new Word(rs.getString("FullWord"), rs.getString("WordWith_"), rs.getString("ImgName"));
+                words.add(newWord);
+            }
+
+            Collections.shuffle(words);
+
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        }
+
+        if (!words.isEmpty()){
+            labelWordToComplete.setText(words.get(0).getWordWith_());
+            Image newImage = new Image(getClass().getResourceAsStream("/imgs/icons/" + words.get(0).getImgName()));
+            MainImage.setImage(newImage);
+        }
     }
 }
---
+++ src/main/java/model/entities/Word.java
@@ -0,0 +1,34 @@
+package model.entities;
+
+public class Word {
+    private String fullWords;
+    private String wordWith_;
+    private String imgName;
+
+    public Word(String fullWords, String wordWith_, String imgName) {
+        this.fullWords = fullWords;
+        this.wordWith_ = wordWith_;
+        this.imgName = imgName;
+    }
+
+    @Override
+    public String toString() {
+        return "Word{" +
+                "fullWords='" + fullWords + '\'' +
+                ", wordWith_='" + wordWith_ + '\'' +
+                ", imgName='" + imgName + '\'' +
+                '}';
+    }
+
+    public String getFullWords() {
+        return fullWords;
+    }
+
+    public String getWordWith_() {
+        return wordWith_;
+    }
+
+    public String getImgName() {
+        return imgName;
+    }
+}
