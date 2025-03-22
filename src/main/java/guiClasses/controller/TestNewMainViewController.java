519fd2a8644ae3df512b20d72fff98098fc4b6cb
Bryan Macedo <contato.bryansm@hotmail.com>
2025-03-22 10:50:06 -0300

    Criei os botões para as vogais e implementei a lógica para completar a palavra.

--- src/main/java/guiClasses/controller/TestNewMainViewController.java
+++ src/main/java/guiClasses/controller/TestNewMainViewController.java
@@ -2,6 +2,8 @@ package guiClasses.controller;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 
@@ -11,6 +13,74 @@ import java.util.ResourceBundle;
 public class TestNewMainViewController implements Initializable {
     @FXML
     private ImageView centerImageView;
+
+    @FXML
+    private Label labelWordToComplete;
+
+    @FXML
+    private Button btLetterA;
+
+    @FXML
+    private Button btLetterE;
+
+    @FXML
+    private Button btLetterI;
+
+    @FXML
+    private Button btLetterO;
+
+    @FXML
+    private Button btLetterU;
+
+    @FXML
+    private void onBtLetterA_Action(){
+        String buttonLetter = "A";
+        String textLabelWordToComplete = labelWordToComplete.getText();
+
+        String newTextLabel = textLabelWordToComplete.replaceFirst("_", buttonLetter);
+
+        labelWordToComplete.setText(newTextLabel);
+
+    }
+
+    @FXML
+    private void onBtLetterE_Action(){
+        String buttonLetter = "E";
+        String textLabelWordToComplete = labelWordToComplete.getText();
+
+        String newTextLabel = textLabelWordToComplete.replaceFirst("_", buttonLetter);
+
+        labelWordToComplete.setText(newTextLabel);
+
+    }@FXML
+    private void onBtLetterI_Action(){
+        String buttonLetter = "I";
+        String textLabelWordToComplete = labelWordToComplete.getText();
+
+        String newTextLabel = textLabelWordToComplete.replaceFirst("_", buttonLetter);
+
+        labelWordToComplete.setText(newTextLabel);
+
+    }@FXML
+    private void onBtLetterO_Action(){
+        String buttonLetter = "O";
+        String textLabelWordToComplete = labelWordToComplete.getText();
+
+        String newTextLabel = textLabelWordToComplete.replaceFirst("_", buttonLetter);
+
+        labelWordToComplete.setText(newTextLabel);
+    }
+
+    @FXML
+    private void onBtLetterU_Action(){
+        String buttonLetter = "U";
+        String textLabelWordToComplete = labelWordToComplete.getText();
+
+        String newTextLabel = textLabelWordToComplete.replaceFirst("_", buttonLetter);
+
+        labelWordToComplete.setText(newTextLabel);
+
+    }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
--- src/main/java/org/bryanmacedo/separacaosilabica/MainTestNewView.java
+++ src/main/java/org/bryanmacedo/separacaosilabica/MainTestNewView.java
@@ -1,14 +1,16 @@
 package org.bryanmacedo.separacaosilabica;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class MainTestNewView extends Application {
-
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/gui/TestNewMainView.fxml"));
