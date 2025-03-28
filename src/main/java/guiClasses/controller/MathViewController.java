1946f1310287fc9bce842b0e3265659ea8f2527d
Bryan Macedo <contato.bryansm@hotmail.com>
2025-03-28 08:35:43 -0300

    Implementei o recurso que verifica a expressão e caso ela seja verdadeira é trocado para a proxima expressão.

--- src/main/java/guiClasses/controller/MathViewController.java
+++ src/main/java/guiClasses/controller/MathViewController.java
@@ -2,6 +2,7 @@ package guiClasses.controller;
 
 import db.DB;
 import db.Exception.DbException;
+import javafx.animation.PauseTransition;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -12,6 +13,7 @@ import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.media.AudioClip;
+import javafx.util.Duration;
 import model.entities.MathematicalExpression;
 import model.entities.TypeMathOperation;
 
@@ -67,21 +69,160 @@ public class MathViewController implements Initializable {
     @FXML
     private void onBtR1Action(){
         labelAnswerUser.setText(btR1.getText());
+        verifyExpression();
     }
 
     @FXML
     private void onBtR2Action(){
         labelAnswerUser.setText(btR2.getText());
+        verifyExpression();
     }
 
     @FXML
     private void onBtR3Action(){
         labelAnswerUser.setText(btR3.getText());
+        verifyExpression();
     }
 
     @FXML
     private void onBtR4Action(){
         labelAnswerUser.setText(btR4.getText());
+        verifyExpression();
+    }
+
+    private void nextExpression(){
+        if (!expressionsList.isEmpty()){
+            String strResult = String.valueOf(expressionsList.get(0).getResult());
+            String strEarly = String.valueOf(expressionsList.get(0).getEarlyExpression());
+            String strType = String.valueOf(expressionsList.get(0).getOperation().getStrOperation());
+            labelMain.setText(strResult);
+            labelEarlyExpression.setText(strEarly);
+            labelOperation.setText(strType);
+            labelAnswerUser.setText("?");
+            btR1.setText(String.valueOf(expressionsList.get(0).getAnswers().get(0)));
+            btR2.setText(String.valueOf(expressionsList.get(0).getAnswers().get(1)));
+            btR3.setText(String.valueOf(expressionsList.get(0).getAnswers().get(2)));
+            btR4.setText(String.valueOf(expressionsList.get(0).getAnswers().get(3)));
+        }else {
+            //chamar a congratulationView
+        }
+    }
+
+    private void verifyExpression(){
+        // desabilitar os botões
+        btR1.setDisable(true);
+        btR2.setDisable(true);
+        btR3.setDisable(true);
+        btR4.setDisable(true);
+
+        String strType = labelOperation.getText();
+
+        int earlyExpression = Integer.parseInt(labelEarlyExpression.getText());
+        int userAnswer = Integer.parseInt(labelAnswerUser.getText());
+        int result = Integer.parseInt(labelMain.getText());
+
+        switch (strType){
+            case "+" ->{
+                if (earlyExpression + userAnswer == result){
+                    labelMain.setStyle(("-fx-text-fill: green;"));
+                    labelEarlyExpression.setStyle(("-fx-text-fill: green;"));
+                    labelOperation.setStyle(("-fx-text-fill: green;"));
+                    labelAnswerUser.setStyle(("-fx-text-fill: green;"));
+
+                    PauseTransition pause = new PauseTransition(Duration.seconds(0.5));
+                    pause.setOnFinished(event -> {
+                        labelMain.setStyle(("-fx-text-fill: white;"));
+                        labelEarlyExpression.setStyle(("-fx-text-fill: white;"));
+                        labelOperation.setStyle(("-fx-text-fill: white;"));
+                        labelAnswerUser.setStyle(("-fx-text-fill: white;"));
+
+                        expressionsList.remove(0);
+
+                        // reabilitar
+                        btR1.setDisable(false);
+                        btR2.setDisable(false);
+                        btR3.setDisable(false);
+                        btR4.setDisable(false);
+
+                        //metodo q troca a expressão
+                        nextExpression();
+                    });
+                    pause.play();
+
+                }else {
+                    labelMain.setStyle(("-fx-text-fill: red;"));
+                    labelEarlyExpression.setStyle(("-fx-text-fill: red;"));
+                    labelOperation.setStyle(("-fx-text-fill: red;"));
+                    labelAnswerUser.setStyle(("-fx-text-fill: red;"));
+
+                    PauseTransition pause = new PauseTransition(Duration.seconds(0.5));
+                    pause.setOnFinished(event -> {
+                        labelMain.setStyle(("-fx-text-fill: white;"));
+                        labelEarlyExpression.setStyle(("-fx-text-fill: white;"));
+                        labelOperation.setStyle(("-fx-text-fill: white;"));
+                        labelAnswerUser.setStyle(("-fx-text-fill: white;"));
+                        labelAnswerUser.setText("?");
+
+                        // reabilitar
+                        btR1.setDisable(false);
+                        btR2.setDisable(false);
+                        btR3.setDisable(false);
+                        btR4.setDisable(false);
+                    });
+                    pause.play();
+                }
+            }
+            case "-" ->{
+                if (earlyExpression - userAnswer == result){
+                    labelMain.setStyle(("-fx-text-fill: green;"));
+                    labelEarlyExpression.setStyle(("-fx-text-fill: green;"));
+                    labelOperation.setStyle(("-fx-text-fill: green;"));
+                    labelAnswerUser.setStyle(("-fx-text-fill: green;"));
+
+                    PauseTransition pause = new PauseTransition(Duration.seconds(0.5));
+                    pause.setOnFinished(event -> {
+                        labelMain.setStyle(("-fx-text-fill: white;"));
+                        labelEarlyExpression.setStyle(("-fx-text-fill: white;"));
+                        labelOperation.setStyle(("-fx-text-fill: white;"));
+                        labelAnswerUser.setStyle(("-fx-text-fill: white;"));
+
+                        expressionsList.remove(0);
+
+                        // reabilitar
+                        btR1.setDisable(false);
+                        btR2.setDisable(false);
+                        btR3.setDisable(false);
+                        btR4.setDisable(false);
+
+                        //metodo q troca a expressão
+                        nextExpression();
+                    });
+                    pause.play();
+
+                }else {
+                    labelMain.setStyle(("-fx-text-fill: red;"));
+                    labelEarlyExpression.setStyle(("-fx-text-fill: red;"));
+                    labelOperation.setStyle(("-fx-text-fill: red;"));
+                    labelAnswerUser.setStyle(("-fx-text-fill: red;"));
+
+                    PauseTransition pause = new PauseTransition(Duration.seconds(0.5));
+                    pause.setOnFinished(event -> {
+                        labelMain.setStyle(("-fx-text-fill: white;"));
+                        labelEarlyExpression.setStyle(("-fx-text-fill: white;"));
+                        labelOperation.setStyle(("-fx-text-fill: white;"));
+                        labelAnswerUser.setStyle(("-fx-text-fill: white;"));
+                        labelAnswerUser.setText("?");
+
+                        // reabilitar
+                        btR1.setDisable(false);
+                        btR2.setDisable(false);
+                        btR3.setDisable(false);
+                        btR4.setDisable(false);
+                    });
+                    pause.play();
+                }
+            }
+        }
     }
 
     @Override
@@ -119,18 +260,9 @@ public class MathViewController implements Initializable {
             e.printStackTrace();
         }
 
-        if (!expressionsList.isEmpty()){
-            String strResult = String.valueOf(expressionsList.get(0).getResult());
-            String strEarly = String.valueOf(expressionsList.get(0).getEarlyExpression());
-            String strType = String.valueOf(expressionsList.get(0).getOperation().getStrOperation());
-            labelMain.setText(strResult);
-            labelEarlyExpression.setText(strEarly);
-            labelOperation.setText(strType);
-            btR1.setText(String.valueOf(expressionsList.get(0).getAnswers().get(0)));
-            btR2.setText(String.valueOf(expressionsList.get(0).getAnswers().get(1)));
-            btR3.setText(String.valueOf(expressionsList.get(0).getAnswers().get(2)));
-            btR4.setText(String.valueOf(expressionsList.get(0).getAnswers().get(3)));
-        }
+        Collections.shuffle(expressionsList);
+        
+        nextExpression();
     }
 
     @FXML
